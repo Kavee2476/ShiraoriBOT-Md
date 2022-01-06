5e8b0335e3d30e89fb48309a35990da89c4bef2d
VIMUKTHI OSHADA <96766297+Kavee2476@users.noreply.github.com>
2022-01-06 15:43:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6281351047727'] // Put your number here
+global.owner = ['94703148103'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -28,10 +28,10 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'ShiraoriBOT Multi device'
-global.author = 'Ilman'
+global.packname = 'KAVEE Multi device'
+global.author = 'VIMUKTHI'
 
-global.wm = '© ShiraoriBOT-Md Made by Ilman'
+global.wm = '© KAVEE-Md Made by VIMUKTHI'
 global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
 
 global.wait = '_*tunggu sedang di proses...*_'
